33ad819c49acd121bad13a1d7eabdfea154cdd8a
ldex <laurent.duveau+git@gmail.com>
2022-03-11 14:32:37 -0500

    Product Insert

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductInsertComponent } from './product-insert/product-insert.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     ProductDetailComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductInsertComponent
   ],
   exports: [ProductListComponent],
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ProductsModule { }
