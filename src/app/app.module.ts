efa74ea83c349b5b5030d1cf6cc9960e905877b9
ldex <laurent.duveau+git@gmail.com>
2022-03-11 10:09:02 -0500

    pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
 import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 
+import { HttpClientModule } from "@angular/common/http";
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +16,8 @@ import { ProductDetailComponent } from './products/product-detail/product-detail
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [ProductListComponent]
--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from '../product.interface';
 
@@ -10,8 +11,30 @@ import { Product } from '../product.interface';
 export class ProductListComponent implements OnInit {
 
   title = "Products";
-  products: Product[];
+  //products: Product[];
+  products$: Observable<Product[]>;
   selectedProduct: Product;
+
+  // Pagination
+  pageSize = 5;
+  start = 0;
+  end = this.pageSize;
+  pageNumber = 1;
+
+  previousPage() {
+    this.start -= this.pageSize;
+    this.end -= this.pageSize;
+    this.pageNumber--;
+    this.selectedProduct = null;
+  }
+
+  nextPage() {
+    this.start += this.pageSize;
+    this.end += this.pageSize;
+    this.pageNumber++;
+    this.selectedProduct = null;
+  }
+
 
   onSelect(product: Product) {
     this.selectedProduct = product;
@@ -22,7 +45,14 @@ export class ProductListComponent implements OnInit {
    }
 
   ngOnInit(): void {
-    this.products = this.productService.getProducts();
+    this.products$ = this.productService.products$;
+
+    // this
+    //   .productService
+    //   .products$
+    //   .subscribe(
+    //     data => this.products = data
+    //   )
   }
 
 }
